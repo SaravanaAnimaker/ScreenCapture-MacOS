3d9652365ebaed0a6c3ee1d12357c863a7617628
Saravana <tsaravanakumar@animaker.com>
2020-09-25 15:46:22 +0530

    Initial Commit

---
+++ ScreenCapture-MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenCapture-MacOS
+//
+//  Created by AnimakerPC167 on 25/09/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
