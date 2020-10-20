69c854f137808a7066b4dcd4fe2419ca2ac9d87c
Saravana <tsaravanakumar@animaker.com>
2020-10-20 10:20:03 +0530

    Create Silent Audio Buffer

--- ScreenCapture-MacOS/RecoderFiles/AMExtension.swift
+++ ScreenCapture-MacOS/RecoderFiles/AMExtension.swift
@@ -83,4 +83,62 @@ extension CMSampleBuffer {
 
     return sampleBufferOut!
   }
+    func createSilentAudio(startFrm: Int64) -> CMSampleBuffer? {
+        let nFrames = CMSampleBufferGetNumSamples(self)
+        let numChannels: UInt32 = 2
+        let sampleRate: Float64 = 44100
+        let bytesPerFrame = UInt32(2 * numChannels)
+        let blockSize = nFrames*Int(bytesPerFrame)
+
+        var block: CMBlockBuffer?
+        var status = CMBlockBufferCreateWithMemoryBlock(
+            kCFAllocatorDefault,
+            nil,
+            blockSize,  // blockLength
+            nil,        // blockAllocator
+            nil,        // customBlockSource
+            0,          // offsetToData
+            blockSize,  // dataLength
+            0,          // flags
+            &block
+        )
+        assert(status == kCMBlockBufferNoErr)
+
+        // we seem to get zeros from the above, but I can't find it documented. so... memset:
+        status = CMBlockBufferFillDataBytes(0, block!, 0, blockSize)
+        assert(status == kCMBlockBufferNoErr)
+
+        var asbd = AudioStreamBasicDescription(
+            mSampleRate: sampleRate,
+            mFormatID: kAudioFormatLinearPCM,
+            mFormatFlags: kLinearPCMFormatFlagIsSignedInteger,
+            mBytesPerPacket: bytesPerFrame,
+            mFramesPerPacket: 1,
+            mBytesPerFrame: bytesPerFrame,
+            mChannelsPerFrame: numChannels,
+            mBitsPerChannel: 16,
+            mReserved: 0
+        )
+
+        var formatDesc: CMAudioFormatDescription?
+        status = CMAudioFormatDescriptionCreate(kCFAllocatorDefault, &asbd, 0, nil, 0, nil, nil, &formatDesc)
+        assert(status == noErr)
+
+        var sampleBuffer: CMSampleBuffer?
+
+        // born ready
+        status = CMAudioSampleBufferCreateReadyWithPacketDescriptions(
+            kCFAllocatorDefault,
+            block,      // dataBuffer
+            formatDesc!,
+            nFrames,    // numSamples
+            CMTimeMake(startFrm, Int32(sampleRate)),    // sbufPTS
+            nil,        // packetDescriptions
+            &sampleBuffer
+        )
+        assert(status == noErr)
+
+        return sampleBuffer
+    }
+
 }
